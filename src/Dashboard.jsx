4cf6de1e64754e70adac746f3c38a7a1a78b0063
Corey Richardson <richardsoncorey04@gmail.com>
2025-07-10 20:46:29 +0100

    Minor capitalisation change

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
                 <div className="links">
                     <a href="https://www.github.com/corey-richardson" target="_blank">GitHub</a>
                     <a href="https://github.com/corey-richardson/archery-tool" target="_blank">Repository</a>
-                    <a href="https://www.linktr.ee/coreyrichardson" target="_blank">LinkTree</a>
+                    <a href="https://www.linktr.ee/coreyrichardson" target="_blank">Linktree</a>
                     <a href="https://wakatime.com/@coreyrichardson/projects/fcdqybsfaa?start=2025-07-05&end=2025-07-10" target="_blank">WakaTime</a>
                     <a href="https://coff.ee/corey.richardson" target="_blank">Buy Me a Coffee</a>
                 </div>
