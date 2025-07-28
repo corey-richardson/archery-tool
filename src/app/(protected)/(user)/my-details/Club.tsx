d41e456f715b3cb016a84eeddf2bf815651d1c80
Corey Richardson <richardsoncorey04@gmail.com>
2025-07-28 22:07:04 +0100

    Club Card starter, and [clubId] route starter

--- src/app/(protected)/(user)/my-details/Club.tsx
+++ src/app/(protected)/(user)/my-details/Club.tsx
@@ -1,12 +1,13 @@
 import { ClubType } from "./ClubType";
+import Link from "next/link";
 
 const Club = ({ club }: { club: ClubType }) => {
     return ( 
-        <>
+        <div className="wider scorecard scorecard-flex">
             { /* Prettify me */ }
-            <p>{ club.name }</p>
+            <Link href={`../club/${club.id}`}><h3>{ club.name }</h3></Link> {/** If Admin, link directly to Members Tools? */}
             { club.membershipDetails.roles.includes('ADMIN') && <p>Admin</p> }
-        </>
+        </div>
      );
 }
  
