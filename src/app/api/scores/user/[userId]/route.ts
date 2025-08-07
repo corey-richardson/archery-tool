7e6b4f5da538fce61037f3cf56d13e59f843b5e0
Corey Richardson <richardsoncorey04@gmail.com>
2025-08-07 16:07:24 +0100

    Fixed route incorrectly requiring admin role

--- src/app/api/scores/user/[userId]/route.ts
+++ src/app/api/scores/user/[userId]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
-import { requireAdminUser } from "@/app/lib/server-utils";
+import { requireLoggedInUser } from "@/app/lib/server-utils";
 
 export async function GET(request: NextRequest, context: any) {
-    await requireAdminUser();
+    await requireLoggedInUser();
 
     const params = await context.params;
     const userId = params.userId;
