53690f70b5bde8ec327fec844aeef4084008aeee
Corey Richardson <richardsoncorey04@gmail.com>
2025-08-11 18:56:32 +0100

    Updated labelling

--- src/app/(protected)/submit-score/page.tsx
+++ src/app/(protected)/submit-score/page.tsx
@@ -13,9 +13,8 @@ async function SubmitScore() {
     return (
         <div className="wider content">
             <h3>Submit a Score to the Records Officer.</h3>
-            <p className="centred">
-                Use this form to submit your latest archery score to your club's records officer. Accurate score submissions help maintain your classification and handicap, and ensure club records are up to date. Please fill in all required fields and double-check your entry before submitting.
-            </p>
+            <p className="centred">Use this form to submit your latest archery score to your club's records officer. Accurate score submissions help maintain your classification and handicap, and ensure club records are up to date. Please fill in all required fields and double-check your entry before submitting.</p>
+            <p className="centred"><br />Note that if your score qualifies for a Bowman level classification and above, or is to be used in a postal/virtual league then your Club Records Officer may also require a countersigned scoresheet to be submitted.</p>
 
             <div className="forms">
                 <ScoreSubmissionForm userId={session?.user.id} />
