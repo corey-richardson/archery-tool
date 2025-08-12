5a250e9dfb5c2adfcdd487f13819dfadb215b121
Corey Richardson <richardsoncorey04@gmail.com>
2025-08-12 13:36:47 +0100

    Suppress eslint warning for unused variable prefixed with `_`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "quotes": ["error", "double", { 
