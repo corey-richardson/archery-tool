9bdfdfb07f906facd7eb573cf0ac7ad2fc17d1be
Corey Richardson <richardsoncorey04@gmail.com>
2025-09-18 18:18:21 +0100

    Remove revalidation idk

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -5,7 +5,6 @@ import "swagger-ui-react/swagger-ui.css";
 import { useEffect, useState } from "react";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 export default function ApiDocsPage() {
     const [ specs, setSpecs ] = useState<any>(null);
